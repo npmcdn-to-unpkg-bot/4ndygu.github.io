94fdf11448eae19e4a28abe53f3ddb9188429790
Andy Gu <andygu@usc.edu>
2016-02-24 23:06:08 -0800

    font and anime

--- js/functions.js
+++ js/functions.js
@@ -1,2 +1,14 @@
 console.log("Hello! This works.");
 // init Slider
+var blink = function() {
+    console.log("BLINK FUCKERS");
+    $('#blinky').animate({
+        opacity: '0'
+    }, function(){
+        $(this).animate({
+            opacity: '1'
+        }, 450, blink);
+    });
+}
+
+blink();
