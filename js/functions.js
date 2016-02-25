7b10284f190b514b3c058f7302523cb910b1b9aa
Andy Gu <andygu@usc.edu>
2016-02-25 11:02:25 -0800

    woops

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,28 @@
 console.log("Hello! This works.");
-// init Slider
+
+//Start doc, init slider
+jQuery(document).ready(function($) {
+	$('#container').imagesLoaded()
+		.always( function( instance ) {
+		  $('.my-slider').unslider({
+		    dots: true,
+		    arrows: false,
+		    fluid: true,
+		    infinite: true,
+		   }).fadeIn();
+	});
+
+	$("#write").typed({
+        strings: ["write.", "watch.", "shoot.", "capture."],
+        typeSpeed: 30,
+        backSpeed: 30,
+        loop: true
+    });
+
+
+});
+
+// init Blinking Text
 var blink = function() {
     console.log("BLINK FUCKERS");
     $('#blinky').animate({
